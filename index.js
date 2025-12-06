8888c2bfee125f74a907034a4bfd947759ab9979
Sylvio PELAGE--MAXIME <sylvio0801@gmail.com>
2025-12-06 19:13:34 +0100

    Finalisation de l'ajout d'un jeu. Création d'une class CModel qui contient des fonctions utile sur la base de donnée. Ajout d'un champ filename dans Game pour les liens d'image

--- index.js
+++ index.js
@@ -4,13 +4,23 @@ const bodyParser = require("body-parser");
 const path = require("path");
 const fs = require("fs");
 const hbs = require("hbs");
+const multer = require("multer");
+
+const {CModel} = require("./scripts/model");
 
 const app = express();
 const prisma = new PrismaClient();
 const PORT = 8080;
 
+const model = new CModel(prisma);
+
+
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static('style'))
+app.use(express.static('uploads'))
 app.set("view engine", "hbs"); // On définit le moteur de template que Express va utiliser
 app.set("views", path.join(__dirname, "views")); // On définit le dossier des vues (dans lequel se trouvent les fichiers .hbs)
 hbs.registerPartials(path.join(__dirname, "views", "partials")); // On définit le dossier des partials (composants e.g. header, footer, menu...)
@@ -21,14 +31,62 @@ hbs.registerHelper("Year", (date) => {
 });
 
 
+app.get("/games/add", async (req, res) => {
+    const editors = await prisma.editor.findMany();
+    const genres = await prisma.genre.findMany();
 
+    res.render(path.join("games","add"),{
+        editors,
+        genres
+    });
+})
+
+
+// Configuration du stockage des fichiers
+const storage = multer.diskStorage({
+    destination: 'uploads/',
+    filename: (req, file, cb) => {
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+    }
+});
+
+const upload = multer({ storage: storage });
+
+app.post("/games", upload.single("file"), async (req, res) => {
+    let {title, releaseDate, desc, genreId, editorId : editorName}  = req.body;
+
+    genreId = +genreId;
+    const editorId = await model.GetIDFromEditorName(editorName);
+    if(editorId === -1)
+    {
+        //FAUT AJOUTER UN EDITOR
+    }
+
+    const name = req.file.filename;
+
+    await prisma.game.create({
+        data : {
+            title,
+            releaseDate : new Date(releaseDate),
+            desc,
+            genreId,
+            editorId,
+            highlighted : true,
+            filename : `${name}`
+        }
+    })
+
+    res.redirect("/");
+});
 
 app.get("/", async (req, res) => {
+
     const games = await prisma.game.findMany({
         where: {
             highlighted: true,
         }
-    })
+    });
+
     res.render("index",{
         games,
         title : "Accueil - Vapeur",
@@ -39,17 +97,15 @@ app.get("/", async (req, res) => {
 })
 
 
-
-
 app.use((req, res, next) => {
     res.status(404).sendFile(path.join(__dirname, "404.html"));
 })
 
 app.use((err, req, res, next) => {
-    console.error(err.stack);
+    console.error(err);
     res.status(500).send("Quelque chose s'est mal passé !");
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port http://localhost:${PORT}`);
 });
---
+++ scripts/model.js
@@ -0,0 +1,43 @@
+const {PrismaClient} = require("@prisma/client");
+
+/**
+ * Class contenant des méthodes pour faire des choses sympa dans la BDD
+ * */
+class CModel
+{
+    /**
+     * @param {PrismaClient} prisma
+     * */
+    constructor(prisma) {
+        this.prisma = prisma;
+    }
+
+    /**
+     * Fonction permettant d'avoir l'ID d'un editor en fonction de son nom
+     * @param {string} editorName
+     * @return {Promise<number>}
+     * */
+    async GetIDFromEditorName(editorName)
+    {
+        try
+        {
+            const editor = await this.prisma.editor.findUnique({
+                where : {
+                    name : editorName
+                }
+            });
+
+            return editor.id;
+        }
+        catch (err)
+        {
+            console.log(err);
+            return -1;
+        }
+
+    }
+}
+
+module.exports = {
+    CModel
+}
